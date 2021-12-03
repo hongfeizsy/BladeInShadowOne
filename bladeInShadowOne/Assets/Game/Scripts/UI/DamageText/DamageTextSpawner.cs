815f5bb8aa99fcb6c4b6960305a1c843a5090262
hongfeizsy <hongfeizsy@gmail.com>
2021-12-03 23:40:32 +0100

    Start to copy the code of DamageTextSpawner.cs from RPGProjectOne.

---
+++ bladeInShadowOne/Assets/Game/Scripts/UI/DamageText/DamageTextSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using RPG.Stats;
+using RPG.Resources;
+using RPG.Combat;
+
+
+namespace RPG.UI.DamageText
+{
+    public class DamageTextSpawner : MonoBehaviour
+    {
+        [SerializeField] DamageText damageTextPrefab = null;
+        
+        public void Spawn()
+        {
+            DamageText textObject = Instantiate(damageTextPrefab, transform.position, Quaternion.identity);
+            textObject.transform.parent = transform;
+            float damageAmount = GetComponentInParent<Health>().GetDamageAmount();
+            //print("Text Spawner: " + damageAmount);
+            textObject.transform.GetChild(0).GetComponentInChildren<Text>().text = string.Format("{0:0}", damageAmount);
+        }
+    }
+}
