c94eeea99c662ff9f2d07fee5bb72a43ed781ea5
hongfeizsy <hongfeizsy@gmail.com>
2021-12-16 23:57:27 +0100

    Insert health bar.

---
+++ bladeInShadowOne/Assets/Game/Scripts/UI/HealthBar/HealthBar.cs
@@ -0,0 +1,32 @@
+﻿using RPG.Resources;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace RPG.UI
+{
+    public class HealthBar : MonoBehaviour
+    {
+        [SerializeField] Image healthBarForeground;
+        [SerializeField] GameObject canvas;
+
+        void Update()
+        {
+            Health health = GetComponentInParent<Health>();
+            bool isDead = health.IsDead();
+            if (!isDead)
+            {
+                canvas.SetActive(true);
+                healthBarForeground.transform.localScale 
+                    = new Vector3(health.GetHealthPercentage() / 100, 1, 1);
+            }
+            else
+            {
+                canvas.SetActive(false);
+                //gameObject.SetActive(false);
+            }
+        }
+    }
+}
