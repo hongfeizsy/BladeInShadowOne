fcb9b45566c012c2e1885f46b516b367afeb7ea8
hongfeizsy <hongfeizsy@gmail.com>
2021-12-24 00:11:53 +0100

    Be able to display the XP level.

---
+++ bladeInShadowOne/Assets/Game/Scripts/UI/ExperienceDisplay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace RPG.Resources
+{
+    public class ExperienceDisplay : MonoBehaviour
+    {
+        GameObject playerObject;
+
+        private void Awake()
+        {
+            playerObject = GameObject.FindGameObjectWithTag("Player");
+        }
+
+        private void Update()
+        {
+            GetComponent<Text>().text = string.Format("{0:N0}", playerObject.GetComponent<Experience>().GetExperiencePoints());
+        }
+    }
+}
